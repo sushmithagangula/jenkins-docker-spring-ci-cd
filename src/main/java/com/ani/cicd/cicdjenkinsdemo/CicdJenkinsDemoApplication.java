cd4ab8d05caabb668c6905c1c109fa1a5c90a515
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-19 10:23:53 +0530

    initial commit

---
+++ src/main/java/com/ani/cicd/cicdjenkinsdemo/CicdJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ani.cicd.cicdjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdJenkinsDemoApplication.class, args);
+	}
+
+}
